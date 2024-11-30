12d074a4402c6162d688df6d3c4073ea67d16573
Abinash Karmakar <codeAbinash@gmail.com>
2024-11-30 23:48:17 +0530

    Implement navigation and theming in the app

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import './src/global.css'
-
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -8,25 +7,85 @@ import './src/global.css'
  */
 
 import Press from '@components/Press'
-import { PaddingBottom, PaddingTop } from '@components/SafePadding'
+import { AutoStatusBar } from '@components/StatusBar'
+import { NavigationContainer } from '@react-navigation/native'
+import { CardStyleInterpolators, createStackNavigator, type StackNavigationOptions } from '@react-navigation/stack'
+import { H, W } from '@utils/dimensions'
 import { SemiBold } from '@utils/fonts'
+import { DarkTheme, DefaultTheme } from '@utils/themes'
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { Dimensions, SafeAreaView, StatusBar, useColorScheme, View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+const IOS_BOTTOM_STYLE: StackNavigationOptions = {
+  cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
+  gestureEnabled: true,
+  gestureDirection: 'vertical',
+  gestureResponseDistance: H,
+}
+
+const NO_ANIMATION: StackNavigationOptions = {
+  cardStyleInterpolator: CardStyleInterpolators.forNoAnimation,
+}
+
+const SMOOTH_ANIMATION: StackNavigationOptions = {
+  cardStyleInterpolator: CardStyleInterpolators.forFadeFromCenter,
+  gestureEnabled: true,
+  gestureDirection: 'horizontal',
+  gestureResponseDistance: H,
+}
+
+type RootStackParamList = {
+  Home: undefined
+}
+
+const GestureEnabled = { gestureEnabled: true }
+
+const Stack = createStackNavigator<RootStackParamList>()
+
+const { width, height } = Dimensions.get('window')
+
+function HomeScreen() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <StatusBar barStyle='dark-content' backgroundColor={'transparent'} />
+      <Press>
+        <SemiBold className='text-4xl text-gray-800 active:text-lime-500 dark:text-white dark:active:text-red-500'>
+          Testbuddy
+        </SemiBold>
+      </Press>
+    </View>
+  )
+}
 
 function App(): React.JSX.Element {
+  const scheme = useColorScheme()
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <StatusBar barStyle='dark-content' backgroundColor={'transparent'} />
-        <PaddingTop />
-        <Press>
-          <SemiBold className='text-4xl text-gray-800 active:text-lime-500'>Testbuddy</SemiBold>
-        </Press>
-        <PaddingBottom />
-      </SafeAreaProvider>
+      <SafeAreaView style={{ height: height, flex: 1 }}>
+        <AutoStatusBar scheme={scheme} />
+        <Navigation />
+      </SafeAreaView>
     </GestureHandlerRootView>
+  )
+}
+
+function Navigation(): React.JSX.Element {
+  return (
+    <NavigationContainer theme={useColorScheme() === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack.Navigator
+        screenOptions={{
+          gestureEnabled: false,
+          gestureDirection: 'horizontal',
+          gestureResponseDistance: W,
+          headerShown: false,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
+        <Stack.Screen name='Home' component={HomeScreen} options={IOS_BOTTOM_STYLE} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
