ea9c5a407bbf14264fe4467ffdc167caed6e87f7
Abinash Karmakar <codeAbinash@gmail.com>
2024-12-07 23:21:44 +0530

    Fix colors in dark mode on the RefreshControl component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,10 +3,13 @@ import { useRefreshByUser } from '@/hooks/useRefreshByUser'
 import { useQuery } from '@tanstack/react-query'
 import { Medium } from '@utils/fonts'
 import type { NavProps } from '@utils/types'
+import { useColorScheme } from 'nativewind'
 import React from 'react'
 import { RefreshControl, ScrollView, StatusBar } from 'react-native'
+import colors from 'tailwindcss/colors'
 
 export default function HomeScreen({ navigation }: NavProps) {
+  const { colorScheme } = useColorScheme()
   const { data, refetch } = useQuery({
     queryKey: ['homeScreen'],
     queryFn: api.homeScreen,
@@ -20,7 +23,13 @@ export default function HomeScreen({ navigation }: NavProps) {
         className='bg-zinc-50 px-5 dark:bg-black'
         contentContainerClassName=''
         refreshControl={
-          <RefreshControl refreshing={isRefetchingByUser} onRefresh={refetchByUser} style={{ zIndex: 1000 }} />
+          <RefreshControl
+            refreshing={isRefetchingByUser}
+            onRefresh={refetchByUser}
+            style={{ zIndex: 1000 }}
+            progressBackgroundColor={colorScheme === 'dark' ? colors.zinc[800] : 'white'}
+            colors={colorScheme === 'dark' ? ['white'] : ['black']}
+          />
         }
       >
         <Medium className='text text-xs'>{JSON.stringify(data, null, 2)}</Medium>
