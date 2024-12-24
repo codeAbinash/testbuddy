cc62f4f1fd94b47b1d75f64616e3007e5cbbb3db
Abinash Karmakar <codeAbinash@gmail.com>
2024-12-24 20:56:25 +0530

    Implement back button confirmation dialog: prompt user before exiting the test and clear test data on confirmation

--- src/screens/Test/Test.tsx
+++ src/screens/Test/Test.tsx
@@ -1,9 +1,11 @@
+import popupStore from '@/zustand/popupStore'
 import api from '@query/api'
 import { RouteProp } from '@react-navigation/native'
 import { useQuery } from '@tanstack/react-query'
 import type { StackNav } from '@utils/types'
 import { useColorScheme } from 'nativewind'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { BackHandler } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
@@ -32,6 +34,8 @@ export default function Test({ navigation, route }: TestProps) {
   const allQn = testStore((store) => store.allQn)
   const qnNo = currentQnStore((store) => store.qnNo)
   const setQnNo = currentQnStore((store) => store.setQnNo)
+  const alert = popupStore((store) => store.alert)
+  const clearTestData = testStore((store) => store.clearTestData)
 
   const { data } = useQuery({
     queryKey: ['test', testId],
@@ -42,6 +46,24 @@ export default function Test({ navigation, route }: TestProps) {
     if (data) setTestData(data)
   }, [data, setTestData])
 
+  useEffect(() => {
+    const onBackPress = () => {
+      alert('Exit test?', 'Do you want to exit the test?', [
+        { text: 'Cancel' },
+        {
+          text: 'YES',
+          onPress: () => {
+            navigation.goBack()
+            clearTestData()
+            setQnNo(0)
+          },
+        },
+      ])
+      return true
+    }
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', onBackPress)
+    return () => backHandler.remove()
+  }, [])
 
   function handleNext() {
     setQnNo((qnNo + 1) % allQn.length)
@@ -53,7 +75,7 @@ export default function Test({ navigation, route }: TestProps) {
 
   return (
     <>
-      <Header navigation={navigation} colorScheme={colorScheme}/>
+      <Header navigation={navigation} colorScheme={colorScheme} />
       <ModalOptions colorScheme={colorScheme} />
       <ScrollView contentContainerClassName='py-3 screen-bg' contentContainerStyle={{ flexGrow: 1 }}>
         <QuestionHeading qnNo={qnNo} allQn={allQn} colorScheme={colorScheme} />
