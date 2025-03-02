3e919a0a9f590a2041be30a272bda7d996c03c66
Abinash Karmakar <codeAbinash@gmail.com>
2025-03-02 12:41:58 +0530

    Refactor PackageComponent to improve styling and selection logic for package tags

--- src/screens/Premium/components/PackageComponent.tsx
+++ src/screens/Premium/components/PackageComponent.tsx
@@ -1,22 +1,27 @@
-import { SemiBold, Medium } from '@utils/fonts';
-import { View, TouchableOpacity } from 'react-native';
-import { PackageProps } from './Premium';
+import { Package } from '@query/api'
+import { Medium, SemiBold } from '@utils/fonts'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+
+export type PackageProps = {
+  item: Package
+  selectedPackage: number
+  setSelectedPackage: (value: number) => void
+  index: number
+}
 
 export function PackageComponent({ item, selectedPackage, setSelectedPackage, index }: PackageProps) {
+  const isSelected = selectedPackage === index
+  const selectedClassName = isSelected ? selectedClassname : defaultClassName
+
   return (
     <View className='relative w-full'>
       <View>
-        <SemiBold
-          className={`absolute z-10 ml-5 rounded-full px-4 ${selectedPackage === index
-              ? 'border-green-500 bg-green-100 text-green-500 dark:bg-green-900'
-              : 'border-zinc-500/20 bg-white text-black dark:border-zinc-500/50 dark:bg-black dark:text-white'}`}
-          style={{ borderWidth: 1.5, fontSize: 9, height: 30, marginTop: -15, lineHeight: 28 }}
-        >
+        <SemiBold className={`absolute z-10 ml-5 rounded-full px-4 ${selectedClassName}`} style={styles.tag}>
           {item.tag}
         </SemiBold>
       </View>
       <TouchableOpacity
-        className={`w-full rounded-2xl p-5 dark:bg-zinc-950 ${selectedPackage === index ? 'border-green-500' : 'border-zinc-500/20 dark:border-zinc-500/50'}`}
+        className={`w-full rounded-2xl p-5 dark:bg-zinc-950 ${isSelected ? 'border-green-500' : 'border-zinc-500/20 dark:border-zinc-500/50'}`}
         onPress={() => setSelectedPackage(index)}
         activeOpacity={0.7}
         style={{ borderWidth: 1.5 }}
@@ -31,5 +36,12 @@ export function PackageComponent({ item, selectedPackage, setSelectedPackage, in
         </Medium>
       </TouchableOpacity>
     </View>
-  );
+  )
 }
+
+const selectedClassname = 'border-green-500 bg-green-100 text-green-500 dark:bg-green-900'
+const defaultClassName = 'border-zinc-500/20 bg-white text-black dark:border-zinc-500/50 dark:bg-black dark:text-white'
+
+const styles = StyleSheet.create({
+  tag: { borderWidth: 1.5, fontSize: 9, height: 30, marginTop: -15, lineHeight: 28 },
+})
