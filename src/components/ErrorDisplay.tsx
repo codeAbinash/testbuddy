d97ce5d185eba849bc1ce9570e1c6fd2c59af4b3
Abinash Karmakar <codeAbinash@gmail.com>
2025-03-17 15:41:05 +0530

    Add loading and error handling to Blogs screen; create ErrorFullScreen component

---
+++ src/components/ErrorDisplay.tsx
@@ -0,0 +1,25 @@
+import Animations from "@assets/animations/animations"
+import { H, W } from "@utils/dimensions"
+import { Medium } from "@utils/fonts"
+import { FC } from "react"
+import { View } from "react-native"
+import { Lottie } from "./Lottie"
+
+
+export const ErrorDisplay: FC = () => {
+  return (
+    <Lottie size={W * 0.5} source={Animations.astronaut} />
+  )
+}
+
+type ErrorFullScreenProps = {
+  text?: string
+}
+export const ErrorFullScreen: FC<ErrorFullScreenProps> = ({ text }) => {
+  return (
+    <View className='flex-1 items-center justify-center' style={{ height: H * 0.7 }}>
+      <ErrorDisplay />
+      {text && <Medium className='text mt-3 text-center text-sm opacity-90'>{text}</Medium>}
+    </View>
+  )
+}
--- src/screens/Blog/Blogs.tsx
+++ src/screens/Blog/Blogs.tsx
@@ -1,16 +1,23 @@
 import Btn from '@components/Button'
+import { ErrorFullScreen } from '@components/ErrorDisplay'
+import { LoadingFullScreen } from '@components/Loading'
 import { PaddingBottom } from '@components/SafePadding'
 import api from '@query/api/api'
 import BackHeader from '@screens/components/BackHeader'
 import { useQuery } from '@tanstack/react-query'
-import type { NavProps } from '@utils/types'
+import type { NavProps, StackNav } from '@utils/types'
+import { FC } from 'react'
 import { ScrollView, View } from 'react-native'
 
 export default function Blogs({ navigation }: NavProps) {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryFn: api.blogs,
     queryKey: ['blogs'],
   })
+
+  if (isLoading) return <LoadingFullScreen text='Loading Blogs' />
+
+  const blogPostId = '6725d3d5b110f3fb01b5754e'
 
   return (
     <>
@@ -18,15 +25,32 @@ export default function Blogs({ navigation }: NavProps) {
       <View className='bg-screen flex-1'>
         <ScrollView contentContainerClassName='bg-screen gap-3 px-3 pb-0 py-5'>
           <Btn
-            title='6725d3d5b110f3fb01b5754e'
-            onPress={() => navigation.navigate('Blog', { id: '6725d3d5b110f3fb01b5754e' })}
+            title={blogPostId}
+            onPress={() => navigation.navigate('Blog', { id: blogPostId })}
           />
-          {data?.map((blog) => (
-            <Btn title={blog.title} key={blog._id} onPress={() => navigation.navigate('Blog', { id: blog._id })} />
-          ))}
+          <List navigation={navigation} data={data} />
           <PaddingBottom />
         </ScrollView>
       </View>
     </>
   )
 }
+
+type ListProps = {
+  navigation: StackNav
+  data: Awaited<ReturnType<typeof api.blogs>> | undefined
+}
+const List: FC<ListProps> = ({ navigation, data }) => {
+  if (data?.isAlert)
+    return <ErrorFullScreen text={data.message || 'Internal Server Error'} />
+
+  return (
+    <>
+      {
+        data?.map((blog) => (
+          <Btn title={blog.title} key={blog._id} onPress={() => navigation.navigate('Blog', { id: blog._id })} />
+        ))
+      }
+    </>
+  )
+} 
